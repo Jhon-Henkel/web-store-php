0eb9ec92bfd8ec7f333f56f515295400a20afb29
Jhonatan Henkel <jhowrf@gmail.com>
2022-06-21 06:58:39 -0300

    Aula 004
    Criado connect, disconnect e select na classe de database, criado constantes referente a conexão com o  db no arquivo de config.

--- config.php
+++ config.php
@@ -2,3 +2,10 @@
 
 const APP_NAME      = 'PhpWebStore';
 const APP_VERSION   = '1.0.0';
+
+//MYSQL
+const MYSQL_SERVER  = 'localhost';
+const MYSQL_DATABASE= 'php_store';
+const MYSQL_USER    = 'root';
+const MYSQL_PASS    = '';
+const MYSQL_CHARSET = 'utf8';
--- core/classes/Database.php
+++ core/classes/Database.php
@@ -1,7 +1,55 @@
 <?php
 namespace core\classes;
 
+use PDO;
+
 class Database
 {
+    private $connectDb;
 
+    private function connectDb()
+    {
+        $this->connectDb = new PDO(
+            'mysql:'
+            . 'host=' . MYSQL_SERVER . ';'
+            . 'dbname=' . MYSQL_DATABASE . ';'
+            . 'charset=' . MYSQL_CHARSET,
+            MYSQL_USER,
+            MYSQL_PASS,
+            array(PDO::ATTR_PERSISTENT => true)
+        );
+
+        //debug
+        $this->connectDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    private function disconnectDb()
+    {
+        $this->connectDb = null;
+    }
+
+    public function select($sql, $params = null)
+    {
+        $this->connectDb();
+
+        $results = null;
+        try {
+
+            $pdo = $this->connectDb->prepare($sql);
+
+            if (!empty($params)) {
+                $pdo->execute($params);
+            }else {
+                $pdo->execute();
+            }
+
+            $results = $pdo->fetchall(PDO::FETCH_CLASS);
+
+        }catch (\PDOException $exception) {
+            return false;
+        }
+
+        $this->disconnectDb();
+        return $results;
+    }
 }
