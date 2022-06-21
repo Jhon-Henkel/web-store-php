6a32ce1b7b03b806ac4c48b51710809c886c6271
Jhonatan Henkel <jhowrf@gmail.com>
2022-06-20 22:51:09 -0300

    Aula 003
    Montado estrutura de pastas base, instalado o composer e feito o autoload.

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+const APP_NAME      = 'PhpWebStore';
+const APP_VERSION   = '1.0.0';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jhowr/web-store',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1833d1fdc787e02b702107f112a3e7a371362b39',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jhowr/web-store' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1833d1fdc787e02b702107f112a3e7a371362b39',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
