0e1a76473969ad02d82ba445e6bb6f9c91eac015
Jhonatan Henkel <jhowrf@gmail.com>
2022-07-23 22:54:19 -0300

    Ajustes de bibliotecas
    Removido bootstrap, fontawesome, jquery, axios e datatables local e colocado via CDN.

--- core/views/admin/layouts/html_header.php
+++ core/views/admin/layouts/html_header.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title><?= APP_NAME . ' ' . APP_VERSION ?></title>
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css"/>
     <link rel="stylesheet" href="../assets/css/app.css">
-    <script type="text/javascript" src="../assets/js/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="../assets/dataTables/datatables.min.css"/>
-    <script type="text/javascript" src="../assets/dataTables/datatables.min.js"></script>
 </head>
 <body>
